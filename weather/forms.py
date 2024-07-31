4b29e57b2958edcf6f934794d1114aa545166653
Sebastian Ortiz <bashortiz18@gmail.com>
2024-07-31 17:46:01 -0500

    validated username and email if it exist in database

--- weather/forms.py
+++ weather/forms.py
@@ -2,10 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,RadioField,PasswordField, SubmitField,BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo,Email, InputRequired, ValidationError
 from weather.weatherData import Weather,ApiKey
+from weather.user import User
+from weather import db
+
+def checkUsernamexist(self,field):
+    if User.query.filter_by(username = field.data).first():
+        raise ValidationError("username already exist")
+
+def checkEmailExist(self,field):
+    if User.query.filter_by(email = field.data).first():
+        raise ValidationError("email already exist")
+
 
 class registrationForm(FlaskForm):
-    userName = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
-    email =  StringField('Email',validators=[DataRequired(), Length(min=2, max=20),Email()])
+    userName = StringField('Username',validators=[DataRequired(), Length(min=2, max=20),checkUsernamexist])
+    email =  StringField('Email',validators=[DataRequired(), Length(min=2, max=20),Email(),checkEmailExist])
     password = PasswordField('Password', validators= [DataRequired()])
     confirmPassword = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
